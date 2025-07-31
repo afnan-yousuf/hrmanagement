4463ffffb16f3c3f62fa0e6db768d029cdfeca55
afnan-yousuf <afnanyousuf@gmail.com>
2025-07-31 16:11:24 +0500

    changes on dashboard

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,6 +4,7 @@ export default function DashboardPage() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-6">Dashboard</h1>
+      <p>This is Admin CI/CD</p>
     </div>
   );
 }
